26e643d7250da11f36a2b52f1a81e91683eb77fe
munirm9786 <121753676+munirm9786@users.noreply.github.com>
2024-11-28 16:18:35 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,8 @@
-
+def test_merge_sort():
+    books = [
+        Book("123", "Python Basics", "John Doe", 2020),
+        Book("456", "Data Structures", "Jane Doe", 2019),
+    ]
+    #
+    sorted_books = merge_sort(books, "name")  
+    assert sorted_books[0].title == "Data Structures"  
