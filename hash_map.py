fb846546506bf33209fc9d91f205d0e3df262335
munirm9786 <121753676+munirm9786@users.noreply.github.com>
2024-11-28 16:10:14 +0000

    Update hash_map.py
    updated

--- hash_map.py
+++ hash_map.py
@@ -1,7 +1,16 @@
+class Book:
+    def __init__(self, isbn, title, author, year):
+        self.isbn = isbn
+        self.title = title
+        self.year = year  
+
+    def __repr__(self):
+        return f"Book(ISBN={self.isbn}, Title={self.isbn}, Year={self.year})"
+
 class HashMap:
     def __init__(self, size=100):
         self.size = size
-        self.buckets = [[] for _ in range(size)]
+        self.buckets = None
 
     def _hash(self, key):
         return hash(key) % self.size
@@ -9,13 +18,12 @@ class HashMap:
     def add_book(self, book):
         index = self._hash(book.isbn)
         for item in self.buckets[index]:
-            if item.isbn = book.isbn:
+            if item.isbn = book.isbn:  
                 return "Book already exists."
         self.buckets[index].append(book)
 
     def search(self, isbn):
         index = self._hash(isbn)
         for book in self.buckets[index]:
-            if book.title == isbn:
                 return book
         return "Book not found."
