ca2e3cb0bcdb7dab4b8e3c9ebc613866bbb614c8
munirm9786 <121753676+munirm9786@users.noreply.github.com>
2024-11-28 16:15:55 +0000

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,1 +1,21 @@
+def merge_sort(books, key):
+    if len(books) == 1:  
+        return books
+    mid = len(books) // 2
+    left = merge_sort(books[:mid], key)
+    right = merge_sort(books[mid:], key)
+    return merge(left, right, key)
 
+def merge(left, right, key):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
