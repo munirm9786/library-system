7818aa2cd66e3eb9f88e89eed343c6b5d7ce1cbb
munirm9786 <121753676+munirm9786@users.noreply.github.com>
2024-12-10 14:15:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 class Book:
     def __init__(self, isbn, title, author, year):
-        """Initialize a Book object."""
         self.isbn = isbn
         self.title = title
         self.author = author
         self.year = year
 
     def __repr__(self):
-        """String representation of a Book."""
         return f"Book(ISBN={self.isbn}, Title={self.title}, Author={self.author}, Year={self.year})"
 
